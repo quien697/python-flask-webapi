a1b76f3a22b152a17bbd4fb4181b8b124b1773ed
Tsung Hsun Liu <quien697@gmail.com>
2020-02-20 19:04:20 +0800

    類型：可與測試專案「react-webapi」做串接

--- api.py
+++ api.py
@@ -5,56 +5,78 @@ from flask import jsonify, request
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Test Data
-tpe = {
-    "id": 0,
-    "city_name": 'Taipei',
-    "country_name": 'Taiwan',
-    "is_capital": True,
-    "location": {
-        "longitude": 121.569649,
-        "latitude": 25.036786
-    }
-}
-nyc = {
-    "id": 1,
-    "city_name": "New York",
-    "country_name": "United States",
-    "is_capital": False,
-    "location": {
-        "longitude": -74.004364,
-        "latitude": 40.710405
-    }
-}
-ldn = {
-    "id": 2,
-    "city_name": "London",
-    "country_name": "United Kingdom",
-    "is_capital": True,
-    "location": {
-        "longitude": -0.114089,
-        "latitude": 51.507497
-    }
-}
-cities = [tpe, nyc, ldn]
+# 跨域支持
+def after_request(resp):
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+app.after_request(after_request)
 
+# City Data 1
+city1 = {
+    "citylist": [
+        {
+            "city_name": "台北", 
+            "country_name": "Taiwan", 
+            "id": 0
+        }, 
+        {
+            "city_name": "New York", 
+            "country_name": "United States", 
+            "id": 1
+        }, 
+        {
+            "city_name": "London", 
+            "country_name": "United Kingdom", 
+            "id": 2
+        }
+    ]
+}
+
+# City Data 2
+city2 = [
+    {
+        "id": 0,
+        "city_name": "Taipei", 
+        "country_name": "台灣", 
+    },
+    {
+        "id": 1,
+        "city_name": "New York", 
+        "country_name": "United States", 
+    },
+    {
+        "id": 2,
+        "city_name": "London", 
+        "country_name": "United Kingdom", 
+    }
+]
+
+# 首頁
 @app.route("/", methods=['GET'])
 def home():
     return "<h1>Hello Quien!</h1>"
 
-@app.route("/cities/all", methods=['GET'])
-def cities_all():
-    return jsonify(cities)
+# 顯示 City List 1
+@app.route("/citylist1", methods=['GET'])
+def citylist1():
+    return jsonify(city1)
 
-@app.route('/cities', methods=['GET'])
-def city_name():
+# 顯示 City List 2
+@app.route("/citylist2", methods=['GET'])
+def citylist2():
+    return jsonify(city2)
+
+# 單獨取得 Case List 2 其中一個項目
+# 例如：http://127.0.0.1:5000/citylist2/item?city_name=Taipei
+@app.route('/citylist2/item', methods=['GET'])
+def city2_name():
     if 'city_name' in request.args:
         city_name = request.args['city_name']
     else:
         return "Error: No city_name provided. Please specify a city_name."
     results = []
 
-    for city in cities:
+    for city in city2:
         if city['city_name'] == city_name:
             results.append(city)
 
